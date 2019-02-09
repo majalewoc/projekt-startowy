a1d23deb98b7299be296dda30c93bb755e4ed947
Maja Lewoc <majalewoc@gmail.com>
2019-02-09 16:43:59 +0100

    Strona internetowa

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+var myHeading =document.querySelector('h1').onmousemove = function(){
+    alert("BRAWO!");
+}
+var myHeading2 = document.querySelector('h2');
+myHeading2.textContent = 'Hello world!';
+var number = 1;
+if (number == 1) {
+    console.log('Warunek Spełniony');
+}else{
+    console.log ('Warunek niespełniony');
+}
+
